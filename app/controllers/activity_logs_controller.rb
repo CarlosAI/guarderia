29d4bf3274fc1eefd52b348f485862d8bebbcd34
CarlosAI <carloswesker@outlook.es>
2020-03-11 23:11:28 -0600

    aver

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,4 +1,28 @@
 class ActivityLogsController < ApplicationController
-  def index
-  end
+	skip_before_action :verify_authenticity_token, :only => [:consultar_info, :index]
+	def index
+	end
+
+	def other_page
+		redirect_to activity_logs_url
+	end
+
+	def consultar_info
+		puts "vamos con consultar info"
+		salida = []
+		parametros = {"baby_id" => params["baby_id"] , "assistant_id" => params["assistant_id"], "status" => params["status"], "page"=> params["page"]}
+		header = {"Content-Type" => "application/json" , "Accept" => "application/json",  "User" => ENV.fetch("User"), "Auth-Token" => ENV.fetch("Auth-Token")}
+		url_base = "https://api-guarderia.herokuapp.com/api/all_activity_logs"
+		response = HTTParty.get("#{url_base}", :query => parametros, :headers =>header)
+		res = JSON.parse response.body
+		if response.code == 200
+			salida = [200, res]
+		else
+			salida = [400, res]
+		end
+
+		respond_to do |format|
+	  		format.json { render json: salida}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'activity_logs/index'
+  get 'activity_logs', to: 'activity_logs#index'
+
+  root 'activity_logs#other_page'
+  post 'consultar_info', to: 'activity_logs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
